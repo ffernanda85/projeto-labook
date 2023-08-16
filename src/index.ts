d5ee186e9fcba6eb7c2aeb2869825c40b637c7d9
ffernanda85 <f.fernanda85@gmail.com>
2023-08-16 16:41:40 -0300

    Develop: ajustes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import cors from "cors"
-import { userRouter } from "./router/usersRouter"
+import { userRouter } from "./router/userRouter"
 import dotenv from 'dotenv'
+import { postRouter } from "./router/postRouter"
 
 dotenv.config()
 
@@ -15,3 +16,4 @@ app.listen(Number(process.env.PORT) || 3003, () => {
 })
 
 app.use("/users", userRouter)
+app.use("/posts", postRouter)
---
+++ src/router/postRouter.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+
+export const postRouter = express.Router()
+
+const postController = new PostController()
